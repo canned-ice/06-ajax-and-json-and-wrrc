f2fccd930bcf3016c614671e4124d3d24e75002a
jlhiskey <jasonhi@crazyredhead.com>
2018-08-21 11:51:05 -0700

    completed the else statement in Article.fetchAll and completed comments.

--- jason-candice/scripts/article.js
+++ jason-candice/scripts/article.js
@@ -12,16 +12,16 @@ function Article (rawDataObj) {
 // REVIEW: Instead of a global `articles = []` array, let's attach this list of all articles directly to the constructor function. Note: it is NOT on the prototype. In JavaScript, functions are themselves objects, which means we can add properties/values to them at any time. In this case, the array relates to ALL of the Article objects, so it does not belong on the prototype, as that would only be relevant to a single instantiated Article.
 Article.all = [];
 
-// COMMENT: Why isn't this method written as an arrow function?
-// PUT YOUR RESPONSE HERE
+// : Why isn't this method written as an arrow function?
+// If we used an arrow function we would lose the instance and this would not point to the object.
 Article.prototype.toHtml = function() {
   let template = Handlebars.compile($('#article-template').text());
 
   this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
 
-  // COMMENT: What is going on in the line below? What do the question mark and colon represent? How have we seen this same logic represented previously?
+  // : What is going on in the line below? What do the question mark and colon represent? How have we seen this same logic represented previously?
   // Not sure? Check the docs!
-  // PUT YOUR RESPONSE HERE
+  // The question mark the of the condition statement and introduces the two expressions that will be return with either true or false. The colon marks an or between the two expressions that could be returned based off of the true or false condition.
   this.publishStatus = this.publishedOn ? `published ${this.daysAgo} days ago` : '(draft)';
   this.body = marked(this.body);
 
@@ -32,8 +32,8 @@ Article.prototype.toHtml = function() {
 
 // REVIEW: This function will take the rawData, how ever it is provided, and use it to instantiate all the articles. This code is moved from elsewhere, and encapsulated in a simply-named function for clarity.
 
-// COMMENT: Where is this function called? What does 'rawData' represent now? How is this different from previous labs?
-// PUT YOUR RESPONSE HERE
+// : Where is this function called? What does 'rawData' represent now? How is this different from previous labs?
+// This function is being call within the Article.fetchAll function below. Now we are just using a property of the Article constructor called .all which contains all of the article data.
 Article.loadAll = articleData => {
   articleData.sort((a,b) => (new Date(b.publishedOn)) - (new Date(a.publishedOn)))
 
@@ -45,9 +45,14 @@ Article.fetchAll = () => {
   // REVIEW: What is this 'if' statement checking for? Where was the rawData set to local storage?
   if (localStorage.rawData) {
 
-    Article.loadAll();
-
-  } else {
-
+    Article.loadAll(localStorage.rawData);
   }
-}
+  else {
+    $.getJSON('/data/hackerIpsum.json')
+      .then(Article => {
+        console.log('Got the Data');
+        Article.loadAll(localStorage.rawData);
+        // When looking at the method fetchAll() we see that the first if statement is looking for local storage data and if it is there then re run method load all. Else if we dont have data then grab it from the "server" which in this case is the data folder.
+      })
+  }
+};
